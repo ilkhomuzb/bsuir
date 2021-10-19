b8b6a9ceec02e807cd02849f8de270d405144872
ilkhomuzb <63558947+ilkhomuzb@users.noreply.github.com>
2021-10-19 11:25:55 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -86,3 +86,44 @@ $('.single-item').slick({
   autoplay: true,
   autoplaySpeed: 5000
 });
+
+$(function(){
+  $("#phone").mask("+375(99) 999-99-99");
+});
+
+$('.dropdown-el').click(function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $(this).toggleClass('expanded');
+  $('#'+$(e.target).attr('for')).prop('checked',true);
+});
+$(document).click(function() {
+  $('.dropdown-el').removeClass('expanded');
+});
+jQuery(($) => {
+  $('.select').on('click', '.select__head', function () {
+    if ($(this).hasClass('open')) {
+      $(this).removeClass('open');
+      $(this).next().fadeOut();
+    } else {
+      $('.select__head').removeClass('open');
+      $('.select__list').fadeOut();
+      $(this).addClass('open');
+      $(this).next().fadeIn();
+    }
+  });
+
+  $('.select').on('click', '.select__item', function () {
+    $('.select__head').removeClass('open');
+    $(this).parent().fadeOut();
+    $(this).parent().prev().text($(this).text());
+    $(this).parent().prev().prev().val($(this).text());
+  });
+
+  $(document).click(function (e) {
+    if (!$(e.target).closest('.select').length) {
+      $('.select__head').removeClass('open');
+      $('.select__list').fadeOut();
+    }
+  });
+});
